3123f3dde0cb285846a2475b396d821f21521779
manoj singh <manojsingh00963@gmail.com>
2025-03-12 15:49:00 +0530

    fix error

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ import Paste from './Paste';
 import ViewPaste from './ViewPaste';
 import SignUp from './SignUp';
 import Login from './Login';
-import Auth from './auth';
+import Auth from './Auth';
 import StartAnimate from './StartAnimate';
 
 export { Navbar, Home, Paste, ViewPaste, SignUp, Login, Auth, StartAnimate};
