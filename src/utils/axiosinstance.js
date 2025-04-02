306f7d1fc7257054a021a8a51b5af78b1d8dd89e
manoj singh <manojsingh00963@gmail.com>
2025-04-02 14:13:29 +0530

    remove console log

--- src/utils/axiosinstance.js
+++ src/utils/axiosinstance.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL ;
 
-
-console.log(API_BASE_URL)
-
 // Create an Axios instance
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
